e4e60e69933c91111e63ecd6b9a931bc73e2f373
Nugie Jaya Nugraha <Nugie Jaya Nugraha>
2022-07-04 23:01:44 +0700

    add button

--- js/script.js
+++ js/script.js
@@ -50,9 +50,16 @@ document.addEventListener(RENDER_EVENT, () => {
     const uncompletedTODOList = document.getElementById('todos');
     uncompletedTODOList.innerHTML = "";
 
+    const completedTODOList = document.getElementById('completed-todos');
+    completedTODOList.innerHTML = "";
+
     for (const todoItem of todos) {
         const todoElement = makeTodo(todoItem);
-        uncompletedTODOList.append(todoElement);
+        if(!todoItem.isCompleted) {
+            uncompletedTODOList.append(todoElement);
+        } else {
+            completedTODOList.append(todoElement);
+        }
     }
   });
 
@@ -74,5 +81,80 @@ const makeTodo = (todoObject) => {
     container.append(textContainer);
     container.setAttribute('id', `todo-${todoObject.id}`);
 
+
+    if (todoObject.isCompleted) {
+        const undoButton = document.createElement('button');
+        undoButton.classList.add('undo-button');
+        undoButton.addEventListener('click', () => {
+            undoTaskFromCompleted(todoObject.id);
+        });
+
+        const trashButton = document.createElement('button');
+        trashButton.classList.add('trash-button');
+        trashButton.addEventListener('click', () => {
+            removeTaskFromCompleted(todoObject.id);
+        });
+
+        container.append(undoButton, trashButton);
+    } else {
+        const checkButton = document.createElement('button');
+        checkButton.classList.add('check-button');
+        checkButton.addEventListener('click', () => {
+            addTaskToCompleted(todoObject.id);
+        });
+
+        container.append(checkButton);
+    }
+
     return container;
+}
+
+
+const addTaskToCompleted = (todoId) => {
+    const todoTarget = findTodo(todoId);
+
+    if (todoTarget == null) return;
+
+    todoTarget.isCompleted = true;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+
+const findTodo = (todoId) => {
+    for (const todoItem of todos) {
+        if (todoItem.id === todoId) {
+            return todoItem;
+        }
+    }
+    return null;
+}
+
+
+const removeTaskFromCompleted = (todoId) => {
+    const todoTarget = findTodoIndex(todoId);
+   
+    if (todoTarget === -1) return;
+   
+    todos.splice(todoTarget, 1);
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+   
+   
+const undoTaskFromCompleted = (todoId) => {
+    const todoTarget = findTodo(todoId);
+   
+    if (todoTarget == null) return;
+   
+    todoTarget.isCompleted = false;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+
+const findTodoIndex = (todoId) => {
+    for (const index in todos) {
+        if (todos[index].id === todoId) {
+            return index;
+        }
+    }
+    return -1;
 }
