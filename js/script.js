724a900cf5d57b7b3de5b279f8cc6365de72c311
Nugie Jaya Nugraha <Nugie Jaya Nugraha>
2022-07-04 21:40:36 +0700

    memahami custom event

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// mematikan reload ketika submit
 document.addEventListener('DOMContentLoaded', () => {
     const submitForm = document.getElementById('form');
     submitForm.addEventListener('submit', (e) => {
@@ -6,10 +7,20 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-const generateID = () => {
+
+// variabel untuk menampung data
+const todos = [];
+// mendefinisikan custom event
+const RENDER_EVENT = 'render-todo';
+
+
+// menghasilkan id unik
+const generateId = () => {
     return +new Date();
 }
 
+
+// membuat objek baru
 const generateTodoObject = (id, task, timestamp, isCompleted) => {
     return {
         id,
@@ -19,15 +30,22 @@ const generateTodoObject = (id, task, timestamp, isCompleted) => {
     }
 }
 
+
 const addTodo = () => {
     const textTodo = document.getElementById('title').value;
     const timestamp = document.getElementById('date').value;
 
-    const generateID = generateID();
+    const generateID = generateId();
     const todoObject = generateTodoObject(generateID, textTodo, timestamp, false);
+
     todos.push(todoObject);
 
     document.dispatchEvent(new Event(RENDER_EVENT));
 }
+
+
+document.addEventListener(RENDER_EVENT, function () {
+    console.log(todos);
+  });
 
 
